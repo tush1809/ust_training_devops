98ab8846367311d03ab697d5145d8a6fba7092c2
Tush1809 <tushar244001@gmail.com>
2025-07-30 18:11:18 +0000

    ek2 notes

---
+++ 30_July/ek2.sh
@@ -0,0 +1,4 @@
+here we learnt about the ek2 cloud how can this we a helpful part 
+___________________________________________________________________>
+
+amazon azure have a serious  competiton but in germany area and other europion regios the azure leads
